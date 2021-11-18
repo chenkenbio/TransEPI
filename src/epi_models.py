7fc1754fe38dd11fcb03d841c79f9da3bc49c710
chenkenbio <chenkenbio@gmail.com>
2021-11-18 19:30:02 +0800

    support variable length

--- src/epi_models.py
+++ src/epi_models.py
@@ -14,118 +14,6 @@ import numpy as np
 from typing import Dict, List
 
 
-# class LstmAttModel(nn.Module):
-#     def __init__(self, in_dim: int, 
-#             cnn_channels: List[int], cnn_sizes: List[int], cnn_pool: List[int],
-#             lstm_size: int, lstm_layer: int, lstm_dropout: float, 
-#             da: int, r: int, att_C: float,
-#             fc: List[int], fc_dropout: float, 
-#             use_dist=False,
-#             **kwargs):
-#         super(LstmAttModel, self).__init__()
-# 
-#         self.cnn = nn.ModuleList()
-#         self.cnn.append(
-#                 nn.Sequential(
-#                     nn.Conv1d(
-#                         in_channels=in_dim, 
-#                         out_channels=cnn_channels[0], 
-#                         kernel_size=cnn_sizes[0], 
-#                         padding=cnn_sizes[0] // 2),
-#                     nn.BatchNorm1d(cnn_channels[0]),
-#                     nn.LeakyReLU(),
-#                     nn.MaxPool1d(cnn_pool[0])
-#                 )
-#             )
-#         for i in range(len(cnn_sizes) - 1):
-#             self.cnn.append(
-#                     nn.Sequential(
-#                         nn.Conv1d(
-#                             in_channels=cnn_channels[i], 
-#                             out_channels=cnn_channels[i + 1], 
-#                             kernel_size=cnn_sizes[i + 1],
-#                             padding=cnn_sizes[i + 1] // 2),
-#                         nn.BatchNorm1d(cnn_channels[i + 1]),
-#                         nn.LeakyReLU(),
-#                         nn.MaxPool1d(cnn_pool[i + 1])
-#                 )
-#             )
-# 
-#         self.lstm = nn.LSTM(
-#                 input_size=cnn_channels[-1],
-#                 hidden_size=lstm_size,
-#                 dropout=lstm_dropout,
-#                 num_layers=lstm_layer,
-#                 bidirectional=True
-#             )
-#         self.lstm_size = lstm_size
-#         self.lstm_layer = lstm_layer
-#         self.lstm_direction = 2
-# 
-#         self.r = r
-#         self.att_C = att_C
-#         self.att_first = nn.Linear(self.lstm_size * self.lstm_direction, da)
-#         self.att_first.bias.data.fill_(0)
-#         self.att_second = nn.Linear(da, r)
-#         self.att_second.bias.data.fill_(0)
-# 
-#         self.use_dist = use_dist
-#         if use_dist:
-#             dist_dim = 1
-#         else:
-#             dist_dim = 0
-#         if fc[-1] != 1:
-#             fc.append(1)
-#         self.fc = nn.ModuleList()
-#         self.fc.append(
-#                 nn.Sequential(
-#                     nn.Dropout(p=fc_dropout),
-#                     nn.Linear(self.lstm_direction * self.lstm_size + dist_dim, fc[0])
-#                 )
-#             )
-#         for i in range(len(fc) - 1):
-#             self.fc.append(
-#                     nn.Sequential(
-#                         nn.ReLU(),
-#                         nn.Linear(fc[i], fc[i + 1])
-#                     )
-#                 )
-#         self.fc.append(nn.Sigmoid())
-# 
-#     def forward(self, feats, dist=None, return_att=False):
-#         batch_size = feats.size(0)
-#         device = feats.device
-#         for cnn in self.cnn:
-#             feats = cnn(feats)
-#         feats = feats.transpose(0, 1).transpose(0, 2)
-#         h0 = Variable(torch.zeros(self.lstm_layer * 2, batch_size, self.lstm_size)).to(device)
-#         c0 = Variable(torch.zeros(self.lstm_layer * 2, batch_size, self.lstm_size)).to(device)
-#         feats, (hn, cn) = self.lstm(feats, (h0, c0))
-#         del h0, c0
-#         feats = feats.transpose(0, 1)
-#         out = torch.tanh(self.att_first(feats))
-#         out = F.softmax(self.att_second(out), 1)
-#         att = out.transpose(1, 2)
-#         del out
-#         seq_embed = torch.matmul(att, feats)
-#         avg_seq_embed = torch.sum(seq_embed, 1) / self.r
-#         del seq_embed, feats
-#         avg_seq_embed = avg_seq_embed.view(batch_size, -1)
-# 
-#         if self.use_dist:
-#             avg_seq_embed = torch.cat((avg_seq_embed, dist), dim=1)
-# 
-#         for fc in self.fc:
-#             avg_seq_embed = fc(avg_seq_embed)
-#         if return_att:
-#             return avg_seq_embed, att
-#         else:
-#             return avg_seq_embed
-# 
-#     def l2_matrix_norm(self, m):                                                                                        
-#         return torch.sum(torch.sum(torch.sum(m**2, 1), 1)**0.5).type(torch.cuda.DoubleTensor)
-
-
 class PositionwiseFeedForward(nn.Module):
     def __init__(self, d_in, d_hid, dropout=0.1):
         super(PositionwiseFeedForward, self).__init__()
@@ -242,6 +130,7 @@ class TransEPI(nn.Module):
                 num_layers=enc_layers
                 )
 
+        self.da = da
         self.r = r
         self.att_C = att_C
         self.att_first = nn.Linear(cnn_channels[-1], da)
@@ -276,7 +165,13 @@ class TransEPI(nn.Module):
 
     def forward(self, feats, enh_idx, prom_idx, return_att=False):
         # feats: (B, D, S)
+        if type(feats) is tuple:
+            feats, length = feats
+        else:
+            length = None
+        div = 1
         for cnn in  self.cnn:
+            div *= cnn[-1].kernel_size
             enh_idx = torch.div(enh_idx, cnn[-1].kernel_size, rounding_mode="trunc")
             prom_idx = torch.div(prom_idx, cnn[-1].kernel_size, rounding_mode="trunc")
             feats = cnn(feats)
@@ -291,6 +186,15 @@ class TransEPI(nn.Module):
             feats = feats.transpose(0, 1)
 
         out = torch.tanh(self.att_first(feats)) # (B, S, da)
+        if length is not None:
+            length = torch.div(length, div, rounding_mode="trunc")
+            max_len = max(length)
+            mask = torch.cat((
+                [torch.cat((torch.ones(1, m, self.da), torch.zeros(1, max_len - m, self.da)), dim=1) for m in length]
+            ), dim=0)
+            assert mask.size() == out.size()
+            out = out * mask.to(out.device)
+            del mask
         out = F.softmax(self.att_second(out), 1) # (B, S, r)
         att = out.transpose(1, 2) # (B, r, S)
         del out
